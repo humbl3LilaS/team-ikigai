023d323404eaf2241abc70543315646d447d340b
Min Khant Kyaw Swar <mkks1986.mkks@gmail.com>
2025-02-09 14:03:13 +0630

    mkks/contact-responsive

--- app/(client)/contact/page.tsx
+++ app/(client)/contact/page.tsx
@@ -7,7 +7,7 @@ const ContactPage = () => {
 
       <div className='block md:flex items-center justify-around md:p-10 gap-10 pb-4 mt-6'>
         <div>
-          <h1 className='font-bold text-3xl md:text-5xl mb-4'>Get In Touch</h1>
+          <h1 className='font-bold flex items-center justify-center text-3xl md:text-5xl mb-4 pb-4 md:pb-10'>Get In Touch</h1>
           <p className='text-xs md:text-sm'>
             Have any questions or need assistance? Contact us through the form below or reach out via phone or email.
           </p>
@@ -20,9 +20,9 @@ const ContactPage = () => {
             <p>support@myantech.com</p>
           </div>
         </div>
-        <div className='mt-6 w-1/3 bg-white p-6 border border-neutral-200 rounded-2xl shadow-md'>
+        <div className='mt-6 w-full md:w-1/3 bg-white p-6 border border-neutral-200 rounded-2xl shadow-md'>
           <h2 className='font-bold text-xl md:text-2xl text-center mb-4'>Send Us a Message</h2>
-          <form className='space-y-4'>
+          <form className='space-y-4'>    
             <input type='text' placeholder='Your Name' className='w-full p-3 border rounded-lg' required />
             <input type='email' placeholder='Your Email' className='w-full p-3 border rounded-lg' required />
             <textarea placeholder='Your Message' className='w-full p-3 border rounded-lg h-32' required></textarea>
