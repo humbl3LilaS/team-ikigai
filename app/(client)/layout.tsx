231b14c45ccbfe39921eb7015659df90ca031bde
KhunAungHtunLay <jotaro097505@gmail.com>
2025-02-09 11:13:33 +0630

    implement product detail page

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -7,7 +7,7 @@ const ClientLayout = ({ children }: { children: ReactNode }) => {
             <div className="fixed top-0 z-50 right-0 left-0">
                 <Header />
             </div>
-            {children}
+            <div className="mt-20">{children}</div>
         </main>
     );
 };
--- components/cards/productCard.tsx
+++ components/cards/productCard.tsx
@@ -1,13 +1,16 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
-import { ShoppingCart } from "lucide-react";
+import { ShoppingCart, ShoppingCartIcon } from "lucide-react";
 
 const ProductCard = () => {
+    const handleAddToCart = () => {};
     return (
         <div className="bg-black/5 p-3 sm:p-5 min-w-52 rounded-md w-full">
-            <Link href="/cart">
+            <Link href="/product/4">
                 <Image
                     src="/laptop.webp"
                     alt="product"
@@ -17,12 +20,30 @@ const ProductCard = () => {
                 />
             </Link>
             <div className="flex flex-col gap-2">
-                <h1 className="">MSI 6x2II</h1>
-                <div className="flex gap-2 items-center cursor-pointer">
+                <Link href="/product/4">
+                    <h1 className="font-semibold py-2">MSI 6x2II</h1>
+                </Link>
+                {/* <div className="flex gap-2 items-center cursor-pointer">
                     <ShoppingCart className="text-blue-600" />
                     <span className="text-sm">Add to cart</span>
-                </div>
-                <Button className="px-10">Buy</Button>
+                </div> */}
+                <p className="text-blue-600 cursor-pointer">
+                    ${" "}
+                    <span className="font-semibold text-sm text-black hover:text-blue-500">
+                        988
+                    </span>
+                </p>
+                <Button
+                    className="px-10"
+                    onClick={() => {
+                        handleAddToCart();
+                    }}
+                >
+                    <span>
+                        <ShoppingCartIcon />
+                    </span>
+                    <p>Add to Cart</p>
+                </Button>
             </div>
         </div>
     );
