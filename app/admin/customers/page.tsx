5c137a39496182dc07e41165e365f5d38ee13404
z_sphinx <soe.thiha.lwin.5@gmail.com>
2025-02-09 15:03:36 +0630

    fix: build error

--- app/admin/customers/page.tsx
+++ app/admin/customers/page.tsx
@@ -1,9 +1,8 @@
-import { D_Users } from "@/dummy";
 import CustomersTable from "@/features/admin/customer/components/customers-table";
 
 
 export default function CustomerPage() {
     return <section className="text-foreground">
-        <CustomersTable columns={D_Users} data={D_Users} />
+        <CustomersTable />
     </section>;
 }
--- features/admin/customer/columns/customers-columns.tsx
+++ features/admin/customer/columns/customers-columns.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { createColumnHelper } from "@tanstack/react-table";
 
 export type TCustomers = {
