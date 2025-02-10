ac7d5dd1c6451412b56cbfaa4e5e732cc09c6fc3
humbl3LilaS <sabishinekobebe@gmail.com>
2025-02-10 19:55:50 +0630

    replace: placeholder data with actual data in /admin/drivers

--- features/admin/driver/components/driver-table.tsx
+++ features/admin/driver/components/driver-table.tsx
@@ -1,6 +1,8 @@
-"use client"
+"use client";
 import {
+    ColumnFiltersState,
     getCoreRowModel,
+    getFilteredRowModel,
     getPaginationRowModel,
     PaginationState,
     useReactTable,
@@ -9,62 +11,92 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useState } from "react";
 
 import DataTableBody from "@/components/share/admin/data-table-body";
+import DataTableSkeleton from "@/components/share/admin/data-table-skeleton";
 import { Button } from "@/components/ui/button";
-import {
-    columns,
-    DRIVER_PLACEHOLDER,
-} from "@/features/admin/driver/columns/driver-columns";
+import { Input } from "@/components/ui/input";
+import { columns } from "@/features/admin/driver/columns/driver-columns";
+import { useGetDrivers } from "@/features/admin/driver/hooks/use-get-drivers";
 
 const DriverTable = () => {
+    const { data: drivers } = useGetDrivers();
     const [pagination, setPagination] = useState<PaginationState>({
         pageIndex: 0,
-        pageSize: 5
-    })
-
+        pageSize: 5,
+    });
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
     const table = useReactTable({
-        data: DRIVER_PLACEHOLDER,
+        data: drivers ?? [],
         columns: columns,
         getCoreRowModel: getCoreRowModel(),
         getPaginationRowModel: getPaginationRowModel(),
         onPaginationChange: setPagination,
+        getFilteredRowModel: getFilteredRowModel(),
+        onColumnFiltersChange: setColumnFilters,
         state: {
             pagination,
+            columnFilters,
         },
     });
 
     return (
         <div className={"p-6 bg-background rounded-2xl relative"}>
             <div>
-                <DataTableBody table={table} data={DRIVER_PLACEHOLDER} />
-                <div className="flex items-center justify-between space-x-2 py-4">
-                    <p className={"text-foreground text-sm font-semibold"}>
-                        Page {pagination.pageIndex + 1} of{" "}
-                        {table.getPageCount()}
-                    </p>
-                    <div>
-                        <Button
-                            variant="outline"
-                            size="sm"
-                            onClick={() => table.previousPage()}
-                            disabled={!table.getCanPreviousPage()}
-                            className={"mr-2"}
-                        >
-                            <ChevronLeft />
-                        </Button>
-                        <Button
-                            variant="outline"
-                            size="sm"
-                            onClick={() => table.nextPage()}
-                            disabled={!table.getCanNextPage()}
-                        >
-                            <ChevronRight />
-                        </Button>
+                {drivers && (
+                    <div className={"mb-4"}>
+                        <Input
+                            placeholder={`Filter by name...`}
+                            value={
+                                (table
+                                    .getColumn("name")
+                                    ?.getFilterValue() as string) ?? ""
+                            }
+                            onChange={(event) =>
+                                table
+                                    .getColumn("name")
+                                    ?.setFilterValue(event.target.value)
+                            }
+                            className="max-w-sm ml-auto"
+                        />
                     </div>
-                </div>
+                )}
+                {!drivers && <DataTableSkeleton paginationOn={true} />}
+                {drivers && (
+                    <DataTableBody table={table} data={drivers ?? []} />
+                )}
+                {drivers && (
+                    <div className="flex items-center justify-between space-x-2 py-4">
+                        <p className={"text-foreground text-sm font-semibold"}>
+                            Page {pagination.pageIndex + 1} of{" "}
+                            {table.getPageCount()}
+                        </p>
+                        <div>
+                            <Button
+                                variant="outline"
+                                size="sm"
+                                onClick={() => {
+                                    table.previousPage();
+                                }}
+                                disabled={!table.getCanPreviousPage()}
+                                className={"mr-2"}
+                            >
+                                <ChevronLeft />
+                            </Button>
+                            <Button
+                                variant="outline"
+                                size="sm"
+                                onClick={() => {
+                                    table.nextPage();
+                                }}
+                                disabled={!table.getCanNextPage()}
+                            >
+                                <ChevronRight />
+                            </Button>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
-    )
-
-}
+    );
+};
 
 export default DriverTable;
