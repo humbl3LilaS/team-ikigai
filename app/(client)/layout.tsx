0e8300be166ea9416717ee1c335050cfd26ca9c7
KhunAungHtunLay <jotaro097505@gmail.com>
2025-02-11 13:03:52 +0630

    implement dynamic metada

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -8,7 +8,7 @@ const ClientLayout = ({ children }: { children: ReactNode }) => {
             <div className="fixed top-0 z-50 right-0 left-0 border-b">
                 <Header />
             </div>
-            <div className="mt-20">{children}</div>
+            <div className="mt-32">{children}</div>
         </main>
     );
 };
--- components/share/client/header.tsx
+++ components/share/client/header.tsx
@@ -9,9 +9,11 @@ import { useState } from "react";
 
 import ProductSearch from "@/components/searchs/product-search";
 import { Button } from "@/components/ui/button";
+import { useCartStore } from "@/features/client/cart/hooks/use-cart-store";
 
 import MobileNav from "./mobile-nav";
-import { useCartStore } from "@/features/client/cart/hooks/use-cart-store";
+import { Input } from "@/components/ui/input";
+
 
 const Header = () => {
     const pathname = usePathname();
@@ -22,15 +24,16 @@ const Header = () => {
     const chars = totalQuantities.toString().slice();
     
     const handleSearch = () => {
-        if (!isSearchOpen) {
+       
             setIsSearchOpen(!isSearchOpen);
-        }
+        
         // implement searching function here
     };
+
     const { data: session } = useSession();
 
     return (
-        <nav className="flex justify-between gap-2 bg-white items-center text-white p-4 sm:px-10 sm:py-6">
+        <nav className="flex max-h-32 justify-between gap-2 bg-white items-center text-white p-4 sm:px-10 sm:py-6">
             <Link href={"/"}>
                 <Image
                     src="/brandLogo.png"
@@ -41,15 +44,17 @@ const Header = () => {
                 />
             </Link>
 
-            <div className={`sm:hidden max-w-md`}>
-                <ProductSearch />
+            <div className={`flex bg-black/25 gap-2 md:hidden px-2 items-center rounded-lg`}>
+                {/* <ProductSearch handleSearch={handleSearch} /> */}
+                <Search/>
+                <input className="bg-transparent text-black focus:outline-none py-1 w-full" />
             </div>
 
-            <div className="sm:hidden">
+            <div className="md:hidden">
                 <MobileNav />
             </div>
 
-            <div className="hidden sm:flex gap-5">
+            <div className="hidden md:flex justify-between gap-5">
                 <div
                     className={`${isSearchOpen ? "sm:hidden" : ""} gap-5 items-center text-black hidden sm:flex font-semibold`}
                 >
@@ -66,7 +71,7 @@ const Header = () => {
                         Contact Us
                     </Link>
                 </div>
-                {isSearchOpen && <ProductSearch />}
+                {isSearchOpen && <ProductSearch handleSearch={handleSearch}  />}
 
                 <Button
                     className="bg-transparent hover:bg-transparent hover:text-blue-500 text-black border"
@@ -76,7 +81,7 @@ const Header = () => {
                 </Button>
                 <Button className="bg-transparent text-black hover:bg-transparent hover:text-blue-500 relative border">
                     <ShoppingCartIcon />
-                    {totalQuantities > 0 && <span className={`absolute -top-2 w-5 h-5 -right-2 text-white bg-red-500 rounded-full ${chars.length < 3 ? 'text-[12px]' : 'text-[10px]'} `}>{totalQuantities}</span>}
+                    {totalQuantities > 0 && <span className={`absolute -top-2 w-5 h-5 -right-2 text-white bg-red-500 rounded-full ${chars.length < 3 ? "text-[12px]" : "text-[10px]"} `}>{totalQuantities}</span>}
                 </Button>
                 {session?.user && (
                     <Link
@@ -89,7 +94,7 @@ const Header = () => {
                 {!session?.user && (
                     <Link
                         href={"/sign-up"}
-                        className="text-black hidden sm:flex font-semibold py-1 items-center border-2 border-black hover:black hover:text-blue-500 hover:bg-transparent rounded-sm"
+                        className="text-black hidden sm:flex font-semibold py-1 px-2 text-[16px] items-center border-2 hover:black hover:text-blue-500 hover:bg-transparent rounded-sm"
                     >
                         Get started
                     </Link>
