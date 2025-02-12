5f939609b9236c23e1520b64b2f6a426f64f033f
humbl3LilaS <sabishinekobebe@gmail.com>
2025-02-12 15:49:34 +0630

    fix: cart summary showing invalid data and checkout form not invalidating orders list

--- features/client/cart/hooks/use-get-cart-summary-info.ts
+++ features/client/cart/hooks/use-get-cart-summary-info.ts
@@ -4,7 +4,7 @@ import { getCartSummary } from "@/features/client/cart/actions/get-cart-summary"
 
 export const useGetCartSummaryInfo = (pids: string[]) => {
     return useQuery({
-        queryKey: ["cart-summary"],
+        queryKey: ["cart-summary", { pids }],
         queryFn: () => getCartSummary(pids),
         enabled: !!pids.length && !!pids.length,
         staleTime: 60 * 60 * 1000,
