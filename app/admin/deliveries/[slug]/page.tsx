9fec809754b8566512347e98e0a7a79f67571275
aung-khantkyaw <aungkhantkyaw.19-20@ucsmub.edu.mm>
2025-02-13 09:56:50 +0630

    add: delivery detail

--- app/admin/deliveries/[slug]/page.tsx
+++ app/admin/deliveries/[slug]/page.tsx
@@ -1,13 +1,15 @@
+import DeliveryDetail from "@/features/admin/delivery/components/delivery-detail";
+
 const DeliveryDetailPage = async ({
     params,
 }: {
     params: Promise<{ slug: string }>;
 }) => {
     const { slug } = await params;
+
     return (
         <div>
-            Delivery Detail Page
-            <p>{slug}</p>
+            <DeliveryDetail id={slug} />
         </div>
     );
 };
