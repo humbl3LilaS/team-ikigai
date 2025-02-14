ed7256cc7a2015ccf63cf247dc7b6945520b3192
KhunAungHtunLay <jotaro097505@gmail.com>
2025-02-14 10:45:07 +0630

    fix: merge error

--- components/share/client/header.tsx
+++ components/share/client/header.tsx
@@ -23,26 +23,30 @@ const Header = async () => {
             </div>
 
 <<<<<<< HEAD
+<<<<<<< HEAD
             <div className="md:hidden flex items-center gap-x-4">
                 <CartLink />
                 <MobileNav isLoggedIn={!!session} role={session?.user.role} />
 =======
 <<<<<<< HEAD
+=======
+>>>>>>> 959e1d8 (fix: merge error)
             <ProductProvider>
                 <div className="md:hidden">
                     <ProductSearch />
                 </div>
             </ProductProvider>
 
-            
-            <div className="md:hidden">
-                <MobileNav isLoggedIn={!!session} />
-=======
+    
+
             <div className="md:hidden flex items-center gap-x-4">
                 <CartLink />
                 <MobileNav isLoggedIn={!!session} role={session?.user.role} />
+<<<<<<< HEAD
 >>>>>>> 7a99742b1e92359f21e45c830985802f7ff30eb5
 >>>>>>> 13fcfd7 (modify landing page ui)
+=======
+>>>>>>> 959e1d8 (fix: merge error)
             </div>
 
             <div className="hidden md:flex justify-between gap-5">
